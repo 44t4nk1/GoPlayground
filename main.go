7aae5aeb264f995bd41de862e51dab27da196e95
SIddharth SIngh <siddharth.singh2019@vitstudent.ac.in>
2020-10-04 17:59:43 +0530

    first REST API

--- main.go
+++ main.go
@@ -1,10 +1,31 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+//Article is
+type Article struct {
+	Title   string `json:"title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+//Articles ...
+type Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{Title: "Test Title1", Desc: "Test Desc1", Content: "Test content1"},
+		Article{Title: "Test Title2", Desc: "Test Desc2", Content: "Test content2"},
+	}
+
+	fmt.Println("Endpont hit: all articles endpoint")
+	json.NewEncoder(w).Encode(articles)
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Homepage endpoint Hit")
@@ -12,6 +33,7 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 func handleRequest() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", allArticles)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
