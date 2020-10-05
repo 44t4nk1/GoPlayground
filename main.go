fea02c842aca7183b97f1e682a22fb6a16f82a23
SIddharth SIngh <siddharth.singh2019@vitstudent.ac.in>
2020-10-05 09:54:24 +0530

    CRUD

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"math"
 	"math/rand"
 	"net/http"
-	"os"
 	"strconv"
 
 	"github.com/gorilla/mux"
@@ -15,18 +14,26 @@ import (
 
 //Article is
 type Article struct {
-	Title   string `json:"title"`
-	Desc    string `json:"desc"`
-	Content string `json:"content"`
+	Title   string  `json:"title"`
+	Desc    string  `json:"desc"`
+	Content string  `json:"content"`
+	Author  *Author `json:"author"`
+}
+
+//Author ...
+type Author struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
 }
 
 //Articles ...
 type Articles []Article
 
 func allArticles(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	articles := Articles{
-		Article{Title: "Test Title1", Desc: "Test Desc1", Content: "Test content1"},
-		Article{Title: "Test Title2", Desc: "Test Desc2", Content: "Test content2"},
+		Article{Title: "Test Title1", Desc: "Test Desc1", Content: "Test content1", Author: &Author{Firstname: "John", Lastname: "Doe"}},
+		Article{Title: "Test Title2", Desc: "Test Desc2", Content: "Test content2", Author: &Author{Firstname: "John", Lastname: "Cena"}},
 	}
 
 	fmt.Println("Endpont hit: all articles endpoint")
@@ -34,6 +41,7 @@ func allArticles(w http.ResponseWriter, r *http.Request) {
 }
 
 func allPostArticles(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	articles := Articles{
 		Article{Title: "Test Titlea", Desc: "Test Desca", Content: "Test contenta"},
 		Article{Title: "Test Titleb", Desc: "Test Descb", Content: "Test contentb"},
@@ -47,11 +55,28 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Homepage endpoint Hit")
 }
 
+func oneArticle(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	articles := Articles{
+		Article{Title: "TestTitle1", Desc: "Test Desc1", Content: "Test content1", Author: &Author{Firstname: "John", Lastname: "Doe"}},
+		Article{Title: "TestTitle2", Desc: "Test Desc2", Content: "Test content2", Author: &Author{Firstname: "John", Lastname: "Cena"}},
+	}
+	params := mux.Vars(r)
+	for _, item := range articles {
+		if item.Title == params["title"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Article{})
+}
+
 func handleRequest() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 
 	myRouter.HandleFunc("/", homePage)
 	myRouter.HandleFunc("/articles", allArticles).Methods("GET")
+	myRouter.HandleFunc("/articles/{title}", oneArticle).Methods("GET")
 	myRouter.HandleFunc("/articles", allPostArticles).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8081", myRouter))
 }
@@ -69,7 +94,7 @@ func concat(a string, b int) string {
 }
 
 func main() {
-	// handleRequest()
+	handleRequest()
 	// playground()
-	fmt.Fprintln(os.Stdout, concat("lol ", 3))
+	// fmt.Println(concat("lol ", 3))
 }
