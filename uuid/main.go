7130dcf8ac91ed5ff11248b67dbf0e6243b62d02
SIddharth SIngh <siddharth.singh2019@vitstudent.ac.in>
2020-11-21 20:15:13 +0530

    peak

--- uuid/main.go
+++ uuid/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"io/ioutil"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
@@ -44,8 +45,21 @@ func CreateBook(c *gin.Context) {
 	c.JSON(200, userCreated)
 }
 
+//GetParams ...
+func GetParams(c *gin.Context) {
+	vars := c.Param("uuid")
+
+	userid, err := uuid.Parse(vars)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Could not parse UUID"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"uuid": userid})
+}
+
 func main() {
 	router := gin.Default()
 	router.POST("/", CreateBook)
+	router.GET("/getter/:uuid", GetParams)
 	router.Run(":8000")
 }
